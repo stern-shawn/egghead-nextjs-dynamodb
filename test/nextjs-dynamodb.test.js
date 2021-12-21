1deeb35b992a940efdc5856e1ad8d8ce81694efb
Shawn Stern <sstern@twilio.com>
2021-12-20 19:11:43 -0800

    initial commit with dynamo provisioning and barebones next app

---
+++ test/nextjs-dynamodb.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const NextjsDynamodb = require('../lib/nextjs-dynamodb-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/nextjs-dynamodb-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new NextjsDynamodb.NextjsDynamodbStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
