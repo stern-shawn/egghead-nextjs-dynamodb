97dca93b5b6036f515c295c9d12a5af64350e129
Shawn Stern <sstern@twilio.com>
2021-12-20 19:39:34 -0800

    add swr to load data to a nextjs page, verify it revalidates on page refocus

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,15 @@
+import useSWR from 'swr';
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 export default function Index() {
-  return 'hello world!';
+  const { data, error } = useSWR(
+    '/api/item?id=461888c5-7dd6-4a79-abb6-c82c90b7a31d',
+    fetcher
+  );
+
+  if (error) return <div>Failed to load</div>;
+  if (!data) return <div>Loading...</div>;
+
+  return <code>{JSON.stringify(data, null, 2)}</code>;
 }
