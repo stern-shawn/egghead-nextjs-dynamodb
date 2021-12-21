33b98b6a77b9d7301df4a2e4d158e5182130509c
Shawn Stern <sstern@twilio.com>
2021-12-20 19:23:22 -0800

    add dynamodb client wrapper and api route that creates items

---
+++ pages/api/item.js
@@ -0,0 +1,16 @@
+import * as uuid from 'uuid';
+import dynamoDb from '../../lib/dynamo';
+
+export default async function handler(req, res) {
+  const item = {
+    id: uuid.v4(),
+    content: 'test',
+    createdAt: Date.now(),
+  };
+
+  await dynamoDb.put({
+    Item: item,
+  });
+
+  res.status(201).json(item);
+}
