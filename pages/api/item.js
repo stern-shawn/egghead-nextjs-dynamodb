74e53a1471b78d6525c72471095b0ea356c066c7
Shawn Stern <sstern@twilio.com>
2021-12-20 19:34:14 -0800

    flesh out remaining CRUD functionality

--- pages/api/item.js
+++ pages/api/item.js
@@ -2,15 +2,52 @@ import * as uuid from 'uuid';
 import dynamoDb from '../../lib/dynamo';
 
 export default async function handler(req, res) {
-  const item = {
-    id: uuid.v4(),
-    content: 'test',
-    createdAt: Date.now(),
-  };
+  if (req.method === 'PUT') {
+    const item = {
+      id: uuid.v4(),
+      content: req.body.content,
+      createdAt: Date.now(),
+    };
 
-  await dynamoDb.put({
-    Item: item,
-  });
+    await dynamoDb.put({
+      Item: item,
+    });
 
-  res.status(201).json(item);
+    res.status(201).json(item);
+  }
+
+  if (req.method === 'GET') {
+    const { Item } = await dynamoDb.get({
+      Key: {
+        id: req.query.id,
+      },
+    });
+
+    res.json(Item);
+  }
+
+  if (req.method === 'POST') {
+    const { Attributes } = await dynamoDb.update({
+      Key: {
+        id: req.body.id,
+      },
+      UpdateExpression: 'SET content = :content',
+      ExpressionAttributeValues: {
+        ':content': req.body.content || null,
+      },
+      ReturnValues: 'ALL_NEW',
+    });
+
+    res.json(Attributes);
+  }
+
+  if (req.method === 'DELETE') {
+    await dynamoDb.delete({
+      Key: {
+        id: req.query.id,
+      },
+    });
+
+    res.status(204).json({});
+  }
 }
